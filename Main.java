2ef4bd6560467e06e115459c6ebd5ea73dbf5475
JerryNYang <46272314+JerryNYang@users.noreply.github.com>
2021-10-09 16:11:20 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+/*
+  We have created a Main class for you
+*/
+public class Main {
+  public static void main(String args[]) {
+    /*
+      Main method
+    */
+    System.out.println("Hello Inyang!!!");
+  }
+}
